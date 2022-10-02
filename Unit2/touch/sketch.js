82c1e12bd765e1d03e376149c6a66ab31d3dfc60
dergberg <97612891+dergberg@users.noreply.github.com>
2022-10-02 16:21:01 -0500

    Add files via upload

--- Unit2/touch/sketch.js
+++ Unit2/touch/sketch.js
@@ -21,21 +21,21 @@ function draw() {
       break ;
       
       case 1: 
-       text("it's kind of lonely here", 5, 22) ; 
+       text("Bloom- who could forget this gem! (kinda sarcasm tho)", 5, 22) ; 
       // put a picture here
       image(i1, 200, 200, 50, 50);
       break ;
       
       case 2:
-      text("two fingers are touching the screen", 5, 22) ; 
+      text("I may or may not have had to do a presentation on SubPop", 5, 22) ; 
             // put a picture here
             image(i2, 200, 200, 50+50, 50);
       break ;
       
       case 3:
-     text("three are touching the screen", 5, 22) ; 
+     text("This album is pretty good", 5, 22) ; 
             // put a picture here
-            image(i3, 200, 200, 50-50, 50);
+            image(i3, 200, 200, 50, 50);
       break ;
     
       
